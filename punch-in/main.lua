2607aae55f2041e8a289001b4f61d4c7f34c60cd
CHEN Qing <chenqing1213@gmail.com>
2017-07-02 22:05:06 +0800

    add notify & repeat script 3 times

--- punch-in/main.lua
+++ punch-in/main.lua
@@ -1,3 +1,15 @@
-runApp('com.alibaba.android.rimet');
-mSleep(10000);
-closeApp('com.alibaba.android.rimet');
+function notify()
+  for var = 1,3 do
+  	vibrator();
+  	mSleep(500);
+  end
+end
+
+
+for var = 1,3 do
+	runApp('com.alibaba.android.rimet');
+	mSleep(10000);
+	closeApp('com.alibaba.android.rimet');
+	notify()
+	mSleep(2000);
+end
--- punch-out/main.lua
+++ punch-out/main.lua
@@ -4,26 +4,26 @@ function click(x, y)
     touchUp(x, y)
 end
 
-runApp('com.alibaba.android.rimet');
+function notify()
+	for var = 1,3 do
+		vibrator();
+		mSleep(500);
+  end
+end
 
-init(0);
-
-setScreenScale(true, 720, 1280)
-
-mSleep(10000);
-
-click(360,1200);
-
-mSleep(3000)
-
-click(95,674);
-
-mSleep(10000); 
-
-click(377,847);
-
-mSleep(5000);
-
-setScreenScale(false)
-
-closeApp('com.alibaba.android.rimet');
+for var = 1,3 do
+	init(0);
+	runApp('com.alibaba.android.rimet');
+	setScreenScale(true, 720, 1280)
+	mSleep(10000);
+	click(360,1200);
+	mSleep(3000)
+	click(95,674);
+	mSleep(10000);
+	click(377,847);
+	mSleep(5000);
+	setScreenScale(false)
+	closeApp('com.alibaba.android.rimet');
+	notify();
+	mSleep(2000);
+end
