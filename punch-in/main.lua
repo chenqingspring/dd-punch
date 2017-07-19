152ff4ec1ec443253c9162f3da1aae51d7b704d2
CHEN Qing <chenqing1213@gmail.com>
2017-07-19 15:14:48 +0800

    add fallbacks

--- punch-in/main.lua
+++ punch-in/main.lua
@@ -37,12 +37,22 @@ for var = 1,3 do
   click(360,1200);
   mSleep(5000)
   x,y = findMultiColorInRegionFuzzy( 0x4da9eb, "0|6|0xffffff,6|6|0x4da9eb,6|18|0x4da9eb,6|21|0xffffff,17|21|0x4da9eb,15|10|0xe0f0fb,15|-1|0xcde6f9,15|-12|0xdeeffb,15|-18|0x4da9eb", 90, 0, 0, 719, 1279);
-  click(x,y);
+  if x ~= -1 and y ~= -1 then
+    click(x,y);
+  else
+    click(93,976);
+  end
   mSleep(20000);
-  x1,y1 = findMultiColorInRegionFuzzy( 0x7baaf7, "0|11|0xffffff,6|8|0x85b0f8,6|-8|0xffffff,6|-16|0x5e97f6,-3|-16|0xffffff,-10|-2|0x5e97f6,-12|10|0xffffff,-17|12|0xf6f9ff,14|12|0xf6f9ff", 90, 0, 0, 719, 1279)
-  click(x1,y1);
-  click(x1,y1);
-  click(x1,y1);
+  x1,y1 = findMultiColorInRegionFuzzy( 0x7baaf7, "0|11|0xffffff,6|8|0x85b0f8,6|-8|0xffffff,6|-16|0x5e97f6,-3|-16|0xffffff,-10|-2|0x5e97f6,-12|10|0xffffff,-17|12|0xf6f9ff,14|12|0xf6f9ff", 90, 0, 0, 719, 1279);
+  if x1 ~= -1 and y1 ~= -1 then
+    click(x1,y1);
+    click(x1,y1);
+    click(x1,y1);
+  else
+    click(375,868);
+    click(375,868);
+    click(375,868);
+  end
   mSleep(10000);
   setScreenScale(false)
   closeApp('com.alibaba.android.rimet');
--- punch-out/main.lua
+++ punch-out/main.lua
@@ -37,12 +37,22 @@ for var = 1,3 do
   click(360,1200);
   mSleep(5000)
   x,y = findMultiColorInRegionFuzzy( 0x4da9eb, "0|6|0xffffff,6|6|0x4da9eb,6|18|0x4da9eb,6|21|0xffffff,17|21|0x4da9eb,15|10|0xe0f0fb,15|-1|0xcde6f9,15|-12|0xdeeffb,15|-18|0x4da9eb", 90, 0, 0, 719, 1279);
-  click(x,y);
+  if x ~= -1 and y ~= -1 then
+    click(x,y);
+  else
+    click(93,976);
+  end
   mSleep(20000);
-  x1,y1 = findMultiColorInRegionFuzzy( 0x5e97f6, "0|-8|0xffffff,0|-12|0x5e97f6,4|6|0xffffff,9|13|0xb0ccfb,9|2|0x5e97f6,20|2|0xffffff,20|19|0xbad2fb,-3|19|0xffffff,-18|-8|0xffffff", 90, 0, 0, 719, 1279);
-  click(x1,y1);
-  click(x1,y1);
-  click(x1,y1);
+  x1,y1 = findMultiColorInRegionFuzzy( 0x5e97f6, "0|-8|0xffffff,0|-12|0x5e97f6,4|6|0xffffff,9|13|0xb0ccfb,9|2|0x5e97f6,20|2|0xffffff,20|19|0xbad2fb,-3|19|0xffffff,-18|-8|0xffffff", 90, 0, 0, 719, 1279);  
+  if x1 ~= -1 and y1 ~= -1 then
+    click(x1,y1);
+    click(x1,y1);
+    click(x1,y1);
+  else
+    click(375,868);
+    click(375,868);
+    click(375,868);
+  end
   mSleep(10000);
   setScreenScale(false)
   closeApp('com.alibaba.android.rimet');
