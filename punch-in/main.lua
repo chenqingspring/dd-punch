7027f73eb0e47d5bae6daeea61f28b568700d1cb
CHEN Qing <chenqing1213@gmail.com>
2017-08-29 16:00:49 +0800

    fixed: 当setScreenScale打开时，将影响点击、取色、单点模糊找色函数

--- punch-in/main.lua
+++ punch-in/main.lua
@@ -35,6 +35,7 @@ for var = 1,3 do
   setScreenScale(true, 720, 1280);
   mSleep(10000);
   click(360,1200);
+  setScreenScale(false)
   mSleep(5000)
   x,y = findMultiColorInRegionFuzzy( 0x4da9eb, "0|6|0xffffff,6|6|0x4da9eb,6|18|0x4da9eb,6|21|0xffffff,17|21|0x4da9eb,15|10|0xe0f0fb,15|-1|0xcde6f9,15|-12|0xdeeffb,15|-18|0x4da9eb", 90, 0, 0, 719, 1279);
   if x ~= -1 and y ~= -1 then
@@ -54,7 +55,6 @@ for var = 1,3 do
     click(375,868);
   end
   mSleep(10000);
-  setScreenScale(false)
   closeApp('com.alibaba.android.rimet');
   notify();
   mSleep(5000);
